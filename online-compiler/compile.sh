0ca7f509891367d2c97ade266064de5eab4cc012
Gopi Pradeep <gopipradeep@users.noreply.huggingface.co>
2025-12-30 10:51:40 +0000

    Delete online-compiler/compile.sh

--- online-compiler/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-SOURCE_FILE=$1
-echo "--- Starting Compiler ---"
-python3 src/lexer.py "$SOURCE_FILE"
-javac src/Parser.java
-(cd go && go run SemanticAnalyzer.go) # Run Go from its own directory
-(cd cpp && g++ CodeGenerator.cpp -o CodeGenerator && ./CodeGenerator) # Run C++ from its own directory
-echo "--- Compilation Successful ---"
